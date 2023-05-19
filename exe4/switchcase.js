d0bd423f3bb4bd26b74a0e758ada4eeb5c62b8b3
Aline Passos <passosaline91@gmail.com>
2023-05-19 19:47:47 -0300

    exercicio 04

---
+++ exe4/switchcase.js
@@ -0,0 +1,18 @@
+let estacaoDoAno = 'outono';
+
+switch(estacaoDoAno) {
+    case 'primavera':
+        console.log('Primavera: Essa época do ano ocorre entre 20 de março a 21 de junho ')
+    break;
+    case 'verao':
+        console.log('Verão: Essa época do ano ocorre entre 21 de junho a 23 de setembro. ')
+    break;
+    case 'outono':
+        console.log('Outono: Essa época do ano ocorre entre 22/23 de setembro a 22 de dezembro')
+    break;
+    case 'inverno':
+        console.log('Inverno: Essa época do ano ocorre entre 22 de dezembro a 20 de março')
+    break;
+    default:
+        console.log('Ops! Nenhuma dessas opções são uma época do ano. Tente novamente.')
+}
