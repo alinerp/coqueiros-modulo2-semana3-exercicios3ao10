bba8dca54ce73839fc537a4b9d70d87fc4fde6fb
Aline Passos <passosaline91@gmail.com>
2023-05-28 15:40:24 -0300

    commit exercicios 6 e 7

---
+++ exe6/while.js
@@ -0,0 +1,6 @@
+var convidados = 10;
+while(convidados > 0) {
+    convidados -= 1;
+    console.log('Convidado atual: ' + convidados);
+}
+console.log('Acabaram os ingressos!')
---
+++ exe7/dowhile.js
@@ -0,0 +1,4 @@
+do {
+    console.log('Tente advinhar o número que estou pensando')
+} while (5 != prompt('Qual número estou pensando?'));
+alert("Acertou!")
